db26369b9cce891854a36916f51c589ddb3a920b
Muhammad Arslan Masood <m_arslan07@yahoo.com>
2025-01-07 15:52:59 +0200

    Transfer files

---
+++ molbert/datasets/dataloading.py
@@ -0,0 +1,426 @@
+import logging
+from typing import Callable
+
+import torch
+from torch.utils.data import DataLoader
+from molbert.datasets.smiles import BertSmilesDataset
+import random
+import numpy as np
+
+
+class MolbertDataLoader(DataLoader):
+    """
+    A custom data loader that does some molbert specific things.
+    1) it skips invalid batches and replaces them with oversampled valid batches such that always n_batches are
+       created.
+    2) it does the valid filtering and trimming in the workers
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # See dataloader.pyi for examplanation of type: ignore
+        self.collate_fn = self.wrapped_collate_fn(self.collate_fn)  # type: ignore
+
+    @staticmethod
+    def trim_batch(batch_inputs, batch_labels):
+        _, cols = torch.where(batch_inputs['attention_mask'] == 1)
+        max_idx: int = int(cols.max().item() + 1)
+
+        for k in ['input_ids', 'token_type_ids', 'attention_mask']:
+            batch_inputs[k] = batch_inputs[k][:, :max_idx].contiguous()
+
+        for k in ['lm_label_ids', 'unmasked_lm_label_ids']:
+            batch_labels[k] = batch_labels[k][:, :max_idx].contiguous()
+        
+
+        return batch_inputs, batch_labels
+
+    def wrapped_collate_fn(self, collate_fn) -> Callable:
+        def collate(*args, **kwargs):
+            batch = collate_fn(*args, **kwargs)
+
+            # valids here is a sequence of valid flags with the same length as the batch
+            (batch_inputs, batch_labels), valids = batch
+            if not valids.all():
+                # filter invalid
+                batch_inputs = {k: v[valids] for k, v in batch_inputs.items()}
+                batch_labels = {k: v[valids] for k, v in batch_labels.items()}
+                # keep trues only to make sure the format is the same
+                valids = valids[valids]
+
+            # whole batch is invalid?
+            if len(valids) == 0:
+                return (None, None), valids
+
+            # trim out excessive padding
+            batch_inputs, batch_labels = self.trim_batch(batch_inputs, batch_labels)
+
+            return (batch_inputs, batch_labels), valids
+
+        return collate
+
+    def __iter__(self):
+        num_batches_so_far = 0
+        num_total_batches = len(self)
+        num_accessed_batches = 0
+
+        while num_batches_so_far < num_total_batches:
+            for (batch_inputs, batch_labels), valids in super().__iter__():
+                num_accessed_batches += 1
+                if len(valids) == 0:
+                    logging.info('EMPTY BATCH ENCOUNTERED. Skipping...')
+                    continue
+                num_batches_so_far += 1
+                yield (batch_inputs, batch_labels), valids
+
+        logging.info(
+            f'Epoch finished. Accessed {num_accessed_batches} batches in order to train on '
+            f'{num_total_batches} batches.'
+        )
+
+#####################################################################################
+# trim both corrupted and clean tokens
+#####################################################################################
+class MolbertDataLoader_corrupted_clean_seq(DataLoader):
+    """
+    A custom data loader that does some molbert specific things.
+    1) it skips invalid batches and replaces them with oversampled valid batches such that always n_batches are
+       created.
+    2) it does the valid filtering and trimming in the workers
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # See dataloader.pyi for examplanation of type: ignore
+        self.collate_fn = self.wrapped_collate_fn(self.collate_fn)  # type: ignore
+
+    @staticmethod
+    def trim_batch(batch_inputs, batch_labels,trim_batch = False):
+        
+        if trim_batch == True:
+            _, cols = torch.where(batch_inputs['attention_mask'] == 1)
+            max_idx: int = int(cols.max().item() + 1)
+
+            for k in ['input_ids', 'token_type_ids', 'attention_mask']:
+                batch_inputs[k] = batch_inputs[k][:, :max_idx].contiguous()
+
+            for k in ['lm_label_ids', 'unmasked_lm_label_ids']:
+                batch_labels[k] = batch_labels[k][:, :max_idx].contiguous()
+        
+
+        return batch_inputs, batch_labels
+
+    def wrapped_collate_fn(self, collate_fn) -> Callable:
+        def collate(*args, **kwargs):
+            batch = collate_fn(*args, **kwargs)
+
+            # valids here is a sequence of valid flags with the same length as the batch
+            (batch_inputs, batch_labels), valids = batch
+            if not valids.all():
+                # filter invalid
+                batch_inputs = {k: v[valids] for k, v in batch_inputs.items()}
+                batch_labels = {k: v[valids] for k, v in batch_labels.items()}
+                # keep trues only to make sure the format is the same
+                valids = valids[valids]
+
+            # whole batch is invalid?
+            if len(valids) == 0:
+                return (None, None), valids
+
+            # trim out excessive padding
+            corrupted_batch_inputs, corrupted_batch_labels = self.trim_batch(batch_inputs[0], batch_labels)
+            clean_batch_inputs, clean_batch_labels = self.trim_batch(batch_inputs[1], batch_labels)
+            
+            for key in corrupted_batch_labels.keys():
+                assert torch.equal(corrupted_batch_labels[key], clean_batch_labels[key]), f"Tensors do not match for key {key}!"
+
+            return ((corrupted_batch_inputs,clean_batch_inputs),  corrupted_batch_labels), valids
+
+        return collate
+
+    def __iter__(self):
+        num_batches_so_far = 0
+        num_total_batches = len(self)
+        num_accessed_batches = 0
+
+        while num_batches_so_far < num_total_batches:
+            for (batch_inputs, batch_labels), valids in super().__iter__():
+                num_accessed_batches += 1
+                if len(valids) == 0:
+                    logging.info('EMPTY BATCH ENCOUNTERED. Skipping...')
+                    continue
+                num_batches_so_far += 1
+                yield (batch_inputs, batch_labels), valids
+
+        logging.info(
+            f'Epoch finished. Accessed {num_accessed_batches} batches in order to train on '
+            f'{num_total_batches} batches.'
+        )
+#####################################################################################
+# Combine two dataloaders
+#####################################################################################
+
+class MolbertDataLoader_for_Multiple_datasets(DataLoader):
+    """
+    A custom data loader that does some MolBERT specific things.
+    1) It skips invalid batches and replaces them with oversampled valid batches such that always n_batches are
+       created.
+    2) It does the valid filtering and trimming in the workers.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.collate_fn = self.wrapped_collate_fn(self.collate_fn)  # type: ignore
+
+    @staticmethod
+    def trim_batch(self,batch_inputs, batch_labels, max_idx):
+
+        # Trim keys based on the maximum sequence length
+        for k in ['input_ids', 'token_type_ids', 'attention_mask']:
+            batch_inputs[k] = batch_inputs[k][:, :max_idx].contiguous()
+
+        for k in ['lm_label_ids', 'unmasked_lm_label_ids']:
+            batch_labels[k] = batch_labels[k][:, :max_idx].contiguous()
+
+        return batch_inputs, batch_labels
+
+    def wrapped_collate_fn(self, collate_fn) -> Callable:
+        def collate(*args, **kwargs):
+            batch = collate_fn(*args, **kwargs)
+
+            (batch_inputs, batch_labels), valids = batch
+
+            if not valids.all():
+                batch_inputs = {k: v[valids] for k, v in batch_inputs.items()}
+                batch_labels = {k: v[valids] for k, v in batch_labels.items()}
+                valids = valids[valids]
+
+            if len(valids) == 0:
+                return (None, None), valids
+
+            #batch_inputs, batch_labels = self.trim_batch(batch_inputs, batch_labels)
+
+            return (batch_inputs, batch_labels), valids
+
+        return collate
+
+    def __iter__(self):
+        return super().__iter__()
+    
+class Combined_invitro_invivo_dataloader:
+    """
+    A custom data loader that combines two MolBERT data loaders.
+    Iterates through the first data loader and cycles through the second data loader.
+    """
+
+    def __init__(self, 
+                 dataloader1: MolbertDataLoader_for_Multiple_datasets, 
+                 dataloader2: MolbertDataLoader_for_Multiple_datasets,
+                 dataloader3: MolbertDataLoader_for_Multiple_datasets,
+                 dataloader4: MolbertDataLoader_for_Multiple_datasets):
+        
+        self.dataloader1 = dataloader1
+        self.dataloader2 = dataloader2
+        self.dataloader3 = dataloader3
+        self.dataloader4 = dataloader4
+
+    def __iter__(self):
+        iter1 = iter(self.dataloader1)
+        iter2 = cycle(self.dataloader2)
+        iter3 = iter(self.dataloader3)
+        iter4 = cycle(self.dataloader4)
+
+        while True:
+            try:
+                batch1 = next(iter1)
+                batch2 = next(iter2)
+                batch3 = next(iter3)
+                batch4 = next(iter4)
+            except StopIteration:
+                break
+
+           
+
+            (batch_inputs1, batch_labels1), valids1 = batch1
+            (batch_inputs2, batch_labels2), valids2 = batch2
+            (batch_inputs3, batch_labels3), valids3 = batch3
+            (batch_inputs4, batch_labels4), valids4 = batch4
+            #print("invitro", batch_inputs1["attention_mask"].shape, batch_inputs3["attention_mask"].shape)
+            #print("invivo", batch_inputs2["attention_mask"].shape, batch_inputs4["attention_mask"].shape)
+
+            if len(valids1) == 0 and len(valids2) == 0 and len(valids3) == 0 and len(valids4) == 0:
+                logging.info('EMPTY BATCH ENCOUNTERED. Skipping...')
+                continue
+            '''
+            # trim batches based on maximum sequence length
+            _, cols = torch.where(batch_inputs1['attention_mask'] == 1)
+            max_idx_1: int = int(cols.max().item() + 1)
+
+            _, cols = torch.where(batch_inputs2['attention_mask'] == 1)
+            max_idx_2: int = int(cols.max().item() + 1)
+
+            _, cols = torch.where(batch_inputs3['attention_mask'] == 1)
+            max_idx_3: int = int(cols.max().item() + 1)
+
+            _, cols = torch.where(batch_inputs4['attention_mask'] == 1)
+            max_idx_4: int = int(cols.max().item() + 1)
+
+
+            max_idx = max(max_idx_1, max_idx_2,max_idx_3, max_idx_4)
+            print("all max", max_idx_1, max_idx_2,max_idx_3, max_idx_4)
+            print("max_idx", max_idx)
+
+            batch_inputs1, batch_labels1 = MolbertDataLoader_for_Multiple_datasets.trim_batch(batch_inputs1, batch_labels1, max_idx)
+            batch_inputs2, batch_labels2 = MolbertDataLoader_for_Multiple_datasets.trim_batch(batch_inputs2, batch_labels2, max_idx)
+            batch_inputs3, batch_labels3 = MolbertDataLoader_for_Multiple_datasets.trim_batch(batch_inputs3, batch_labels3, max_idx)
+            batch_inputs4, batch_labels4 = MolbertDataLoader_for_Multiple_datasets.trim_batch(batch_inputs4, batch_labels4, max_idx)
+
+            print("invitro_trim", batch_inputs1["attention_mask"].shape, batch_inputs3["attention_mask"].shape)
+            print("invivo_trim ", batch_inputs2["attention_mask"].shape, batch_inputs4["attention_mask"].shape)
+            '''
+            yield [((batch_inputs1, batch_labels1), valids1), 
+                   ((batch_inputs2, batch_labels2), valids2),
+                   ((batch_inputs3, batch_labels3), valids3),
+                   ((batch_inputs4, batch_labels4), valids4)]
+
+        logging.info('Epoch finished.')
+
+    def __len__(self):
+        return len(self.dataloader1)
+    
+class CombinedMolbertDataLoader_max:
+    """
+    A custom data loader that combines two MolBERT data loaders.
+    Iterates through the first data loader and cycles through the second data loader.
+    """
+
+    def __init__(self, 
+                 dataloader1: MolbertDataLoader_for_Multiple_datasets, 
+                 dataloader2: MolbertDataLoader_for_Multiple_datasets,
+                 dataloader3: MolbertDataLoader_for_Multiple_datasets,
+                 dataloader4: MolbertDataLoader_for_Multiple_datasets):
+        
+        self.dataloader1 = dataloader1
+        self.dataloader2 = dataloader2
+        self.dataloader3 = dataloader3
+        self.dataloader4 = dataloader4
+
+    def __iter__(self):
+        iter1 = iter(self.dataloader1)
+        iter2 = iter(self.dataloader2)
+        iter3 = iter(self.dataloader3)
+        iter4 = iter(self.dataloader4)
+
+        while True:
+            try:
+                batch1 = next(iter1)
+            except StopIteration:
+                break
+
+            batch2 = next(iter2, None)
+            batch3 = next(iter3, None)
+            batch4 = next(iter4, None)
+           
+
+            (batch_inputs1, batch_labels1), valids1 = ((None, None), None) if batch1 is None else batch1
+            (batch_inputs2, batch_labels2), valids2 = ((None, None), None) if batch2 is None else batch2
+            (batch_inputs3, batch_labels3), valids3 = ((None, None), None) if batch3 is None else batch3
+            (batch_inputs4, batch_labels4), valids4 = ((None, None), None) if batch4 is None else batch4
+
+            if all(v is None for v in [valids1, valids2, valids3, valids4]):
+                logging.info('EMPTY BATCH ENCOUNTERED. Skipping...')
+                continue
+            '''
+            # trim batches based on maximum sequence length
+            max_idx_1 = (int(torch.where(batch_inputs1['attention_mask'] == 1)[1].max().item() + 1) 
+                         if batch_inputs1 is not None else 1)
+
+            max_idx_2 = (int(torch.where(batch_inputs2['attention_mask'] == 1)[1].max().item() + 1) 
+                         if batch_inputs2 is not None else 1)
+
+            max_idx_3 = (int(torch.where(batch_inputs3['attention_mask'] == 1)[1].max().item() + 1) 
+                         if batch_inputs3 is not None else 1)
+
+            max_idx_4 = (int(torch.where(batch_inputs4['attention_mask'] == 1)[1].max().item() + 1) 
+                         if batch_inputs4 is not None else 1)
+
+
+            max_idx = max(max_idx_1, max_idx_2,max_idx_3, max_idx_4)
+            if batch_inputs1 is not None:
+                batch_inputs1, batch_labels1 = MolbertDataLoader_for_Multiple_datasets.trim_batch(batch_inputs1, batch_labels1, max_idx)
+            if batch_inputs2 is not None:
+                batch_inputs2, batch_labels2 = MolbertDataLoader_for_Multiple_datasets.trim_batch(batch_inputs2, batch_labels2, max_idx)
+            if batch_inputs3 is not None:
+                batch_inputs3, batch_labels3 = MolbertDataLoader_for_Multiple_datasets.trim_batch(batch_inputs3, batch_labels3, max_idx)
+            if batch_inputs4 is not None:
+                batch_inputs4, batch_labels4 = MolbertDataLoader_for_Multiple_datasets.trim_batch(batch_inputs4, batch_labels4, max_idx)
+            '''
+            yield [((batch_inputs1, batch_labels1), valids1), 
+                   ((batch_inputs2, batch_labels2), valids2),
+                   ((batch_inputs3, batch_labels3), valids3),
+                   ((batch_inputs4, batch_labels4), valids4)]
+
+        logging.info('Epoch finished.')
+
+    def __len__(self):
+        return len(self.dataloader1)
+    
+
+def get_dataloaders(featurizer, targets, num_workers,config_dict, train_shuffle = True):
+
+    if targets == "invitro":
+        train_input_path = config_dict['invitro_train']
+        val_input_path = config_dict['invitro_val']
+        label_column = config_dict["invitro_columns"]
+        num_tasks = config_dict["num_invitro_tasks"]
+        batch_size = config_dict["invitro_batch_size"]
+
+    if targets == "invivo":
+        train_input_path = config_dict['invivo_train']
+        val_input_path = config_dict['invivo_val']
+        label_column = config_dict["invivo_columns"]
+        num_tasks = config_dict["num_invivo_tasks"]
+        batch_size = config_dict["invivo_batch_size"]
+
+
+    train_dataset = BertSmilesDataset(
+            input_path= train_input_path,
+            featurizer= featurizer,
+            single_seq_len= config_dict["max_seq_length"],
+            total_seq_len= config_dict["max_seq_length"],
+            label_column= label_column,
+            is_same= config_dict["is_same_smiles"],
+            num_invitro_tasks = num_tasks,
+            num_physchem= config_dict["num_physchem_properties"],
+            permute= config_dict["permute"],
+            named_descriptor_set=config_dict["named_descriptor_set"],
+            inference_mode = False
+        )
+
+    validation_dataset = BertSmilesDataset(
+                input_path= val_input_path,
+                featurizer= featurizer,
+                single_seq_len= config_dict["max_seq_length"],
+                total_seq_len= config_dict["max_seq_length"],
+                label_column= label_column,
+                is_same= config_dict["is_same_smiles"],
+                num_invitro_tasks = num_tasks,
+                num_physchem= config_dict["num_physchem_properties"],
+                permute= config_dict["permute"],
+                named_descriptor_set=config_dict["named_descriptor_set"],
+                inference_mode = False
+            )
+
+    train_dataloader = MolbertDataLoader_corrupted_clean_seq(train_dataset, 
+                                        batch_size=batch_size,
+                                        pin_memory=False,
+                                        num_workers=num_workers, 
+                                        shuffle = train_shuffle)
+
+    val_dataloader = MolbertDataLoader_corrupted_clean_seq(validation_dataset, 
+                                        batch_size=batch_size,
+                                        pin_memory=False,
+                                        num_workers=num_workers, 
+                                        shuffle = False)
+    
+    return  train_dataloader, val_dataloader
---
+++ molbert/datasets/smiles.py
@@ -0,0 +1,151 @@
+import logging
+import random
+from typing import List, Optional, Tuple, Union
+import pandas as pd
+
+import torch
+from molbert.datasets.base import BaseBertDataset
+from molbert.utils.featurizer.molfeaturizer import PhysChemFeaturizer
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+
+
+class BertSmilesDataset(BaseBertDataset):
+    def __init__(
+        self,
+        input_path,
+        featurizer,
+        single_seq_len,
+        total_seq_len,
+        num_invitro_tasks = 0,
+        num_physchem=0,
+        permute=False,
+        named_descriptor_set='all',
+        label_column = None,
+        *args,
+        **kwargs,
+    ):
+
+        super().__init__(input_path, featurizer, single_seq_len, total_seq_len,*args, **kwargs)
+        self.num_invitro_tasks = num_invitro_tasks
+        self.num_physchem = num_physchem
+        self.permute = permute
+        self.physchem_featurizer: PhysChemFeaturizer
+
+        if self.num_physchem > 0:
+            descriptor_list = PhysChemFeaturizer.get_descriptor_subset(named_descriptor_set, self.num_physchem)
+            self.physchem_featurizer = PhysChemFeaturizer(descriptors=descriptor_list, normalise=True)
+
+        #########################################
+        # Change from original code
+        #########################################
+        if self.num_invitro_tasks > 0:
+            if input_path.endswith('.csv'):
+                data = pd.read_csv(input_path)
+            elif input_path.endswith('.pkl'):
+                data = pd.read_pickle(input_path)
+            else:
+                raise ValueError("Unsupported file format. Please provide a CSV or pickle file.")
+            data = data.fillna(-1)
+            self.labels = data[label_column].values
+
+    def calculate_physchem_props(self, smiles: str):
+        physchem, valid = self.physchem_featurizer.transform_single(smiles)
+        assert bool(valid), f'Cannot compute the physchem props for {smiles}'
+
+        return physchem[: self.num_physchem]
+
+    def get_sequence(
+        self, index: int, permute_sample: bool = False, *args, **kwargs
+    ) -> Tuple[Optional[List[str]], bool]:
+        """
+        Returns a permuted SMILES given a position index
+        """
+
+        sequence = self.sequences[index]
+        self.original_smiles = self.sequences[index]
+        #print("permutatons", permute_sample, self.permute)
+        if sequence is None:
+            return None, False
+
+        if self.permute or permute_sample:
+            permutation = self.featurizer.permute_smiles(smiles_str=sequence)
+
+        else:
+            permutation = self.featurizer.standardise(sequence)
+            permutation = sequence
+
+        if permutation is None or not self.featurizer.is_legal(permutation):
+            return None, False
+
+        return permutation, True
+
+    def get_related_seq(self, index: int, max_retries: int = 10) -> Tuple[Optional[List[str]], bool]:
+        """
+        Returns a permuted SMILES given a position index
+        """
+        # related seq is always permuted
+        seq, valid = self.get_sequence(index, permute_sample=True)
+
+        return seq, valid
+
+    def get_unrelated_seq(self, avoid_idx: int, max_retries: int = 10) -> Tuple[Optional[List[str]], bool]:
+        """
+        Tries to returns a permuted SMILES different than the one in input
+        """
+        for _ in range(max_retries):
+
+            index = random.randint(0, len(self) - 1)
+
+            if index == avoid_idx:
+                continue
+
+            seq, valid = self.get_sequence(index, permute_sample=True)
+
+            if valid:
+                return seq, True
+
+        return None, False
+
+    def prepare_sample(
+        self,
+        cur_id: int,
+        t1: Optional[Union[List[str], str]],
+        t2: Optional[Union[List[str], str]] = None,
+        is_same: Optional[bool] = None,
+    ):
+
+        assert t1 is not None
+
+        encoded_tokens_a = list(self.featurizer.encode(t1))
+        encoded_tokens_b = list(self.featurizer.encode(t2)) if t2 else None
+
+        inputs, labels = super().prepare_sample(cur_id, encoded_tokens_a, encoded_tokens_b, is_same)
+
+        if self.num_physchem > 0:
+            # get the original sequence of the sample to support cache indexing
+            sequence = str(self.sequences[cur_id])
+            physchem_properties = self.calculate_physchem_props(sequence)
+            labels['physchem_props'] = torch.tensor(physchem_properties, dtype=torch.float)
+        
+        #########################################
+        # Change from original code
+        #########################################
+        if self.num_invitro_tasks > 0:
+            labels['invitro'] = torch.tensor(self.labels[cur_id], dtype=torch.float).unsqueeze(0)
+
+
+        return inputs, labels
+
+    def get_invalid_sample(self):
+        print("######smiles class invalid sample#############")
+        inputs, labels = super().get_invalid_sample()
+
+        if self.num_physchem > 0:
+            labels['physchem_props'] = torch.zeros(self.num_physchem, dtype=torch.float)
+
+        if self.num_invitro_tasks > 0:
+            labels['invitro'] = -torch.ones(self.num_invitro_tasks, dtype=torch.float).unsqueeze(0)
+
+        return inputs, labels
